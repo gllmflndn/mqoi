d0d8875943854ad63adab5e5caefac19c6423306
Guillaume Flandin <guillaume.flandin@gmail.com>
2021-12-22 14:55:59 +0000

    Test QOI decoder

---
+++ test_qoiread.m
@@ -0,0 +1,23 @@
+function test_qoiread (data_dir)
+
+if ~nargin
+    data_dir = pwd;
+end
+
+files = dir (fullfile (data_dir, '*.qoi'));
+files = files(~[files.isdir]);
+for i=1:numel (files)
+    fprintf ('Reading "%s"... ',files(i).name);
+    try
+        IMG1 = qoiread (fullfile (files(i).folder, files(i).name));
+        [IMG2, ~, A] = imread (fullfile (files(i).folder, [files(i).name(1:end-3) 'png']));
+        if ~isempty (A), IMG2 = cat (3, IMG2, A); end
+        if isequal(IMG1,IMG2)
+            fprintf ('pass\n');
+        else
+            fprintf ('fail\n');
+        end
+    catch
+        fprintf ('error\n');
+    end
+end
